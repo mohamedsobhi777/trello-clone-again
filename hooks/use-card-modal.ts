af70ba21036867f0804c7de26154f0bb06ce7a4f
msmhmorsi <msmhmorsi@connect.ust.hk>
2023-12-28 22:16:32 +0800

    finish cards functionality

--- hooks/use-card-modal.ts
+++ hooks/use-card-modal.ts
@@ -1,13 +1,13 @@
 import { create } from "zustand";
 
-type MobileSidebarStore = {
+type CardModalStore = {
     id?: string;
     isOpen: boolean;
     onOpen: (id: string) => void;
     onClose: () => void;
 };
 
-export const useMobileSidebar = create<MobileSidebarStore>((set) => ({
+export const useCardModal = create<CardModalStore>((set) => ({
     id: undefined,
     isOpen: false,
     onOpen: (id: string) => set({ isOpen: true, id }),
