e7bea8524e8d21af000a566f88e25db5bafc8748
msmhmorsi <msmhmorsi@connect.ust.hk>
2023-12-28 14:49:34 +0800

    implemented dnd

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { ListWithCards } from "@/types";
+import ListHeader from "./list-header";
+import { ElementRef, useRef, useState } from "react";
+import { CardForm } from "./card-form";
+import { cn } from "@/lib/utils";
+import CardItem from "./card-item";
+import { Droppable, Draggable } from "@hello-pangea/dnd";
+
+interface ListeItemProps {
+    data: ListWithCards;
+    index: number;
+
+}
+
+const ListItem = ({ data, index }: ListeItemProps) => {
+    const textareaRef = useRef<ElementRef<"textarea">>(null);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const disableEditing = () => {
+        setIsEditing(false);
+    }
+
+    const enableEditing = () => {
+        setIsEditing(true);
+        setTimeout(() => {
+            textareaRef.current?.focus();
+        })
+    }
+
+    return (
+        <Draggable draggableId={data.id} index={index}>
+            {(provided) => (
+
+                <li
+                    {...provided.draggableProps}
+                    ref={provided.innerRef}
+                    className="shrink-0 h-full w-[272] select-none"
+                >
+                    <div {...provided.dragHandleProps} className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
+                        <ListHeader
+                            onAddCard={enableEditing}
+                            data={data}
+                        />
+                        <Droppable droppableId={data.id} type="card" >
+                            {(provided) => (
+                                <ol
+                                    ref={provided.innerRef}
+                                    {...provided.droppableProps}
+                                    className={cn("mx-1 px-1 py-0.5 flex flex-col gap-y-2", data.cards.length > 0 ? 'mt-2' : 'mt-2')}
+                                >
+                                    {data.cards.map(
+                                        (card, index) =>
+                                            <CardItem
+                                                index={index}
+                                                key={card.id}
+                                                data={card}
+                                            />
+                                    )}
+                                    {provided.placeholder}
+                                </ol>
+                            )}
+                        </Droppable>
+                        <CardForm
+                            listId={data.id}
+                            ref={textareaRef}
+                            isEditing={isEditing}
+                            enableEditing={enableEditing}
+                            disableEditing={disableEditing}
+                        />
+                    </div>
+                </li>
+            )}
+        </Draggable>
+    )
+}
+
+export default ListItem
--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,9 +1,43 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ListContainer } from "./_components/list-container";
 
-type Props = {}
+interface BoardIdProps {
+    params: {
+        boardId: string;
+    }
+}
 
-const BoardIdPage = (props: Props) => {
+const BoardIdPage = async ({ params }: BoardIdProps) => {
+    const { orgId } = auth();
+    if (!orgId) redirect("/select-org");
+
+    const lists = await db.list.findMany({
+        where: {
+            boardId: params.boardId,
+            board: {
+                orgId
+            }
+        },
+        include: {
+            cards: {
+                orderBy: {
+                    order: "asc"
+                }
+            }
+        },
+        orderBy: {
+            order: "asc"
+        }
+    });
     return (
-        <div>BoardIdPage</div>
+        <div className="p-4 h-full overflow-x-auto">
+            <ListContainer
+                boardId={params.boardId}
+                data={lists}
+            />
+        </div>
     )
 }
 
